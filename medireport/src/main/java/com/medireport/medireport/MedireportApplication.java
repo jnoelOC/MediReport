7d5c51359f17da576384090b62732186c1279051
Jean-Noël CHAMBE <jean-noel.chambe@visiativ.com>
2023-03-21 11:22:38 +0100

    first commit

---
+++ medireport/src/main/java/com/medireport/medireport/MedireportApplication.java
@@ -0,0 +1,13 @@
+package com.medireport.medireport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedireportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedireportApplication.class, args);
+	}
+
+}
---
+++ medireport/src/test/java/com/medireport/medireport/MedireportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medireport.medireport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedireportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
