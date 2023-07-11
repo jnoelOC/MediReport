64c3da91d9da3cf0d8b87ca686b9b03ad1e45919
Jean-Noël CHAMBE <jnchambe@hotmail.com>
2023-07-11 10:11:11 +0200

    Adding curls for report

--- mediscreen/medireport/src/main/java/com/medireport/medireport/web/controller/curl/ReportCurlController.java
+++ mediscreen/medireport/src/main/java/com/medireport/medireport/web/controller/curl/ReportCurlController.java
@@ -32,6 +32,72 @@ public class ReportCurlController {
         this.patientsProxy = patientsProxy;
     }
 
+    @PostMapping("/report/curl/assess/id")
+    public ResponseEntity<String> recupererNiveauDeRisqueDuPatientParId(Integer idPatient) {
+        logger.info("dans la méthode recupererNiveauDeRisqueDuPatientParId de medireport");
+
+        PatientBean patient = patientsProxy.recupererUnPatient(idPatient);
+        if(null == patient){
+            logger.error("Dans recupererNiveauDeRisqueDuPatientParId de medireport : patient Non trouvé.");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND.toString(), HttpStatus.NOT_FOUND);
+        }
+
+        List<NoteBean> notes = notesProxy.listerLesNotesParPatient(idPatient);
+
+        Disease.RiskLevel riskLevel = diseaseService.getRiskLevel(notes, patient);
+
+        if(null == riskLevel){
+            logger.error("Dans recupererNiveauDeRisqueDuPatientParId de medireport : riskLevel Non trouvé.");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND.toString(), HttpStatus.NOT_FOUND);
+        }
+        else{
+            logger.info("Dans recupererNiveauDeRisqueDuPatientParId, riskLevel trouvé.");
+            String result = " Patient : " + patient.getFirstname() +
+                    " (age=" + LocalDate.now().minusYears(patient.getDob().getYear()).getYear() +
+                    ") diabetes assessment is : " + riskLevel.toString();
+            return new ResponseEntity<>(result, HttpStatus.FOUND);
+        }
+    }
+
+    @PostMapping("/report/curl/assess/familyName")
+    public ResponseEntity<String> recupererNiveauDeRisqueParNom(String familyName) {
+        logger.info("dans la méthode recupererNiveauDeRisqueParNom de medireport");
+        PatientBean patient = null;
+        Disease.RiskLevel riskLevel = null;
+
+        Optional<Integer> idPatientOptional = patientsProxy.listerLesPatients().stream()
+                .filter(onePatient -> onePatient.getFirstname().equals(familyName))
+                .map(PatientBean::getId)
+                .findFirst();
+
+        Integer idPatient = idPatientOptional.orElse(null);
+
+        if (null != idPatient) {
+            patient = patientsProxy.recupererUnPatient(idPatient);
+            if (null == patient) {
+                logger.error("Dans recupererNiveauDeRisqueParNom de medireport : patient Non trouvé.");
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND.toString(), HttpStatus.NOT_FOUND);
+            }
+
+            List<NoteBean> notes = notesProxy.listerLesNotesParPatient(idPatient);
+
+            riskLevel = diseaseService.getRiskLevel(notes, patient);
+        }
+
+        if(null == riskLevel){
+            logger.error("Dans recupererNiveauDeRisqueParNom de medireport : riskLevel Non trouvé.");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND.toString(), HttpStatus.NOT_FOUND);
+        }
+        else{
+            logger.info("Dans recupererNiveauDeRisqueParNom, riskLevel trouvé.");
+            String result = " Patient : " + patient.getFirstname() +
+                    " (age=" + LocalDate.now().minusYears(patient.getDob().getYear()).getYear() +
+                    ") diabetes assessment is : " + riskLevel.toString();
+            return new ResponseEntity<>(result, HttpStatus.FOUND);
+        }
+    }
+
+
     @GetMapping("/report/curl/get")
     public ResponseEntity<String> recupererNiveauDeRisqueDuPatient(Integer idPatient) {
         logger.info("dans la méthode recupererNiveauDeRisqueDuPatient de medireport");
@@ -52,7 +118,7 @@ public class ReportCurlController {
         }
         else{
             logger.info("Dans recupererNiveauDeRisqueDuPatient, riskLevel trouvé.");
-            String result = " Patient : " + patient.getLastname() +
+            String result = " Patient : " + patient.getFirstname() +
                     " (age=" + LocalDate.now().minusYears(patient.getDob().getYear()).getYear() +
                     ") diabetes assessment is : " + riskLevel.toString();
             return new ResponseEntity<>(result, HttpStatus.FOUND);
@@ -67,7 +133,7 @@ public class ReportCurlController {
         Disease.RiskLevel riskLevel = null;
 
         Optional<Integer> idPatientOptional = patientsProxy.listerLesPatients().stream()
-                .filter(onePatient -> onePatient.getLastname().equals(familyName))
+                .filter(onePatient -> onePatient.getFirstname().equals(familyName))
                 .map(PatientBean::getId)
                 .findFirst();
 
@@ -91,7 +157,7 @@ public class ReportCurlController {
         }
         else{
             logger.info("Dans recupererNiveauDeRisqueDuPatient, riskLevel trouvé.");
-            String result = " Patient : " + patient.getLastname() +
+            String result = " Patient : " + patient.getFirstname() +
                     " (age=" + LocalDate.now().minusYears(patient.getDob().getYear()).getYear() +
                     ") diabetes assessment is : " + riskLevel.toString();
             return new ResponseEntity<>(result, HttpStatus.FOUND);
